27c7444e35af6bbba109af167dd61041da293326
Mike Wallbank <m.wallbank@sheffield.ac.uk>
2017-06-28 07:37:26 -0500

    initial commin of PIDAnaAlg, alg class which fill tree used to analyse the MVAPIDResults and identify separations

--- dune/FDSelections/NueCutSelection_module.cc
+++ dune/FDSelections/NueCutSelection_module.cc
@@ -7,6 +7,7 @@
 // from cetlib version v3_00_01.
 ////////////////////////////////////////////////////////////////////////
 
+// framework includes
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
@@ -16,18 +17,18 @@
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art/Framework/Services/Optional/TFileService.h"
 
-//STL
+// LArSoft includes
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "nusimdata/SimulationBase/MCFlux.h"
+#include "PIDAnaAlg.h"
+
+// c++
 #include <iostream>
+
 //ROOT
 #include "TTree.h"
-//ART
-#include "art/Framework/Services/Optional/TFileService.h"
-//LArSoft
-#include "nusimdata/SimulationBase/MCTruth.h"
-
-
-
 
 constexpr int kDefInt = -9999;
 constexpr int kDefDoub = (double)(kDefInt);
@@ -36,12 +37,10 @@ namespace FDSelection {
   class NueCutSelection;
 }
 
-
 class FDSelection::NueCutSelection : public art::EDAnalyzer {
 public:
-  explicit NueCutSelection(fhicl::ParameterSet const & p);
-  // The compiler-generated destructor is fine for non-base
-  // classes without bare pointers or other resource use.
+
+  explicit NueCutSelection(const fhicl::ParameterSet& p);
 
   // Plugins should not be copied or assigned.
   NueCutSelection(NueCutSelection const &) = delete;
@@ -50,46 +49,47 @@ public:
   NueCutSelection & operator = (NueCutSelection &&) = delete;
 
   // Required functions.
-  void analyze(art::Event const & e) override;
+  void analyze(const art::Event& e) override;
 
   // Selected optional functions.
   void beginJob() override;
-  void beginSubRun(art::SubRun const & sr) override;
+  void beginSubRun(const art::SubRun& sr) override;
   void endJob() override;
 
 private:
 
-  //Delcare private functions
-  void Reset();      //Resets all tree vars
+  /// Resets all tree variables
+  void Reset();
 
-  void GetTruthInfo(art::Event const & evt);  //Grab the truth info from the art record
-  // Declare member data here.
+  /// Get the truth info from the art event record
+  void GetTruthInfo(const art::Event& evt);
 
-  TTree *fTree; //The selection tree
-  //Generic stuff
+  // The selection tree
+  TTree *fTree;
+  // Generic stuff
   int fRun;
   int fSubRun;
   int fEvent;
   int fIsMC;
-  //Neutrino stuff
-  int fNuPdg; //Interaction PDG
-  int fBeamPdg; //PDG at point of creation
-  int fNC;    // 1=is NC, 0=otherwise
-  int fMode; // 0=QE/El, 1=RES, 2=DIS, 3=Coherent production
+  // Neutrino stuff
+  int fNuPdg;   // Interaction PDG
+  int fBeamPdg; // PDG at point of creation
+  int fNC;      // 1=is NC, 0=otherwise
+  int fMode;    // 0=QE/El, 1=RES, 2=DIS, 3=Coherent production
   double fQ2; 
   double fEnu; 
-  double fW; //X-Sec params
+  double fW; // X-Sec params
   double fX;
   double fY;
-  double fNuMomX; //Neutrino momentums
+  double fNuMomX; // Neutrino momentums
   double fNuMomY;
   double fNuMomZ;
   double fNuMomT;
-  double fNuX; //Interaction positions
+  double fNuX; // Interaction positions
   double fNuY;
   double fNuZ;
   double fNuT;
-  //Outgoing lepton stuff
+  // Outgoing lepton stuff
   int fLepPDG;
   double fLepMomX;
   double fLepMomY;
@@ -97,125 +97,177 @@ private:
   double fLepMomT;
   double fLepNuAngle;
 
-  //Fhicl pset labels
-  std::string fNuGenModuleLabel;
+  // Module fhicl labels
+  std::string fNuGenModuleLabel, fTrackModuleLabel, fShowerModuleLabel, fPIDModuleLabel;
 
- 
+  // Algs
+  PIDAnaAlg fPIDAnaAlg;
+
 };
 
+DEFINE_ART_MODULE(FDSelection::NueCutSelection)
 
-FDSelection::NueCutSelection::NueCutSelection(fhicl::ParameterSet const & pset)
-  :
-  EDAnalyzer(pset)   ,
-  fNuGenModuleLabel        (pset.get< std::string >("NuGenModuleLabel"))
-{}
+FDSelection::NueCutSelection::NueCutSelection(const fhicl::ParameterSet& pset) : EDAnalyzer(pset),
+                                                                                 fPIDAnaAlg(pset) {
+  fNuGenModuleLabel  = pset.get<std::string>("NuGenModuleLabel");
+  fTrackModuleLabel  = pset.get<std::string>("TrackModuleLabel");
+  fShowerModuleLabel = pset.get<std::string>("ShowerModuleLabel");
+  fPIDModuleLabel    = pset.get<std::string>("PIDModuleLabel");
+}
 
-void FDSelection::NueCutSelection::analyze(art::Event const & evt)
-{
-  //Get the generic stuff that can be pulled from the top of the record
+void FDSelection::NueCutSelection::analyze(const art::Event& evt) {
+
+  // Generic stuff that can be pulled from the top of the record
   fRun = evt.run();
   fSubRun = evt.subRun();
   fEvent = evt.event();
   fIsMC = !(evt.isRealData());
 
-  if (fIsMC) GetTruthInfo(evt);
+  if (fIsMC)
+    GetTruthInfo(evt);
+
+  fPIDAnaAlg.Run(evt);
 
   fTree->Fill();
   Reset(); //Reset at the end of the event
+
+  return;
+
 }
 
-void FDSelection::NueCutSelection::beginJob()
-{
-  // Implementation of optional member function here.
+void FDSelection::NueCutSelection::beginJob() {
+
     art::ServiceHandle<art::TFileService> tfs;
     fTree = tfs->make<TTree>("nuecutsel","Nue cut selection");
-    fTree->Branch("Run",&fRun);
-    fTree->Branch("SubRun",&fSubRun);
-    fTree->Branch("Event",&fEvent);
-    fTree->Branch("IsMC",&fIsMC);
-    fTree->Branch("NuPdg",&fNuPdg);
-    fTree->Branch("BeamPdg",&fBeamPdg);
-    fTree->Branch("NC",&fNC);
-    fTree->Branch("Mode",&fMode);
-    fTree->Branch("Q2",&fQ2);
-    fTree->Branch("Enu",&fEnu);
-    fTree->Branch("W",&fW);
-    fTree->Branch("X",&fX);
-    fTree->Branch("Y",&fY);
-    fTree->Branch("NuMomX",&fNuMomX);
-    fTree->Branch("NuMomY",&fNuMomY);
-    fTree->Branch("NuMomZ",&fNuMomZ);
-    fTree->Branch("NuMomT",&fNuMomT);
-    fTree->Branch("NuX",&fNuX);
-    fTree->Branch("NuY",&fNuY);
-    fTree->Branch("NuZ",&fNuZ);
-    fTree->Branch("NuT",&fNuT);
-    fTree->Branch("LepPDG",&fLepPDG);
-    fTree->Branch("LepMomX",&fLepMomX);
-    fTree->Branch("LepMomY",&fLepMomY);
-    fTree->Branch("LepMomZ",&fLepMomZ);
-    fTree->Branch("LepMomT",&fLepMomT);
-    fTree->Branch("LepNuAngle",&fLepNuAngle);
+    fTree->Branch("Run",	&fRun);
+    fTree->Branch("SubRun",	&fSubRun);
+    fTree->Branch("Event",	&fEvent);
+    fTree->Branch("IsMC",	&fIsMC);
+    fTree->Branch("NuPdg",	&fNuPdg);
+    fTree->Branch("BeamPdg",	&fBeamPdg);
+    fTree->Branch("NC",		&fNC);
+    fTree->Branch("Mode",	&fMode);
+    fTree->Branch("Q2",		&fQ2);
+    fTree->Branch("Enu",	&fEnu);
+    fTree->Branch("W",		&fW);
+    fTree->Branch("X",		&fX);
+    fTree->Branch("Y",		&fY);
+    fTree->Branch("NuMomX",	&fNuMomX);
+    fTree->Branch("NuMomY",	&fNuMomY);
+    fTree->Branch("NuMomZ",	&fNuMomZ);
+    fTree->Branch("NuMomT",	&fNuMomT);
+    fTree->Branch("NuX",	&fNuX);
+    fTree->Branch("NuY",	&fNuY);
+    fTree->Branch("NuZ",	&fNuZ);
+    fTree->Branch("NuT",	&fNuT);
+    fTree->Branch("LepPDG",	&fLepPDG);
+    fTree->Branch("LepMomX",	&fLepMomX);
+    fTree->Branch("LepMomY",	&fLepMomY);
+    fTree->Branch("LepMomZ",	&fLepMomZ);
+    fTree->Branch("LepMomT",	&fLepMomT);
+    fTree->Branch("LepNuAngle",	&fLepNuAngle);
 
-    Reset();  //Default value all variables now
+    Reset();
+
 }
 
-void FDSelection::NueCutSelection::beginSubRun(art::SubRun const & sr)
-{
+void FDSelection::NueCutSelection::beginSubRun(art::SubRun const & sr) {
   // Implementation of optional member function here.
 }
 
-void FDSelection::NueCutSelection::endJob()
-{
+void FDSelection::NueCutSelection::endJob() {
   // Implementation of optional member function here.
 }
 
-void FDSelection::NueCutSelection::Reset()
-{
-  //Generic stuff
-  fRun = kDefInt;
-  fSubRun = kDefInt;
-  fEvent = kDefInt;
-  fIsMC = kDefInt;
-  //Neutrino stuff
-  fNuPdg = kDefInt; 
-  fBeamPdg = kDefInt; 
-  fNC = kDefInt;    
-  fMode = kDefInt; 
-  fQ2 = kDefDoub; 
-  fEnu = kDefDoub; 
-  fW = kDefDoub; 
-  fX = kDefDoub;
-  fY = kDefDoub;
-  fNuMomX = kDefDoub; 
-  fNuMomY = kDefDoub;
-  fNuMomZ = kDefDoub;
-  fNuMomT = kDefDoub;
-  fNuX = kDefDoub; 
-  fNuY = kDefDoub;
-  fNuZ = kDefDoub;
-  fNuT = kDefDoub;
-  //Outgoing lepton stuff
-  fLepPDG = kDefInt;
-  fLepMomX = kDefDoub;
-  fLepMomY = kDefDoub;
-  fLepMomZ = kDefDoub;
-  fLepMomT = kDefDoub;
-  fLepNuAngle = kDefDoub;
+void FDSelection::NueCutSelection::Reset() {
+  // Generic stuff
+  fRun		= kDefInt;
+  fSubRun	= kDefInt;
+  fEvent	= kDefInt;
+  fIsMC		= kDefInt;
+  // Neutrino stuff
+  fNuPdg	= kDefInt; 
+  fBeamPdg	= kDefInt; 
+  fNC		= kDefInt;    
+  fMode		= kDefInt; 
+  fQ2		= kDefDoub; 
+  fEnu		= kDefDoub; 
+  fW		= kDefDoub; 
+  fX		= kDefDoub;
+  fY		= kDefDoub;
+  fNuMomX	= kDefDoub; 
+  fNuMomY	= kDefDoub;
+  fNuMomZ	= kDefDoub;
+  fNuMomT	= kDefDoub;
+  fNuX		= kDefDoub; 
+  fNuY		= kDefDoub;
+  fNuZ		= kDefDoub;
+  fNuT		= kDefDoub;
+  // Outgoing lepton stuff
+  fLepPDG	= kDefInt;
+  fLepMomX	= kDefDoub;
+  fLepMomY	= kDefDoub;
+  fLepMomZ	= kDefDoub;
+  fLepMomT	= kDefDoub;
+  fLepNuAngle	= kDefDoub;
 }
 
-void FDSelection::NueCutSelection::GetTruthInfo(art::Event const & evt){
-  //Get the truth record
-  art::Handle< std::vector<simb::MCTruth> > mcTruthListHandle;
-  std::vector<art::Ptr<simb::MCTruth> > mcList;
-  if (evt.getByLabel(fNuGenModuleLabel, mcTruthListHandle)){
-    art::fill_ptr_vector(mcList, mcTruthListHandle);
+void FDSelection::NueCutSelection::GetTruthInfo(const art::Event& evt){
+
+  // Get the truth record
+  art::Handle<std::vector<simb::MCTruth> > mcTruthHandle;
+  std::vector<art::Ptr<simb::MCTruth> > mcTruth;
+  if (evt.getByLabel(fNuGenModuleLabel, mcTruthHandle)){
+    art::fill_ptr_vector(mcTruth, mcTruthHandle);
   }
-  //Chuck out a warning if there are multiple truths (i.e. multiple neutrinos)
-  if (mcList.size() > 1){
-    mf::LogWarning("NueCutSelection") << "There are  " << mcList.size() << " MCTruth in this event.  Only taking the first one!!!!";
+  else
+    mf::LogWarning("NueCutSelection") << "No MCTruth";
+
+  // Get the flux record
+  art::Handle<std::vector<simb::MCFlux> > fluxHandle;
+  std::vector<art::Ptr<simb::MCFlux> > flux;
+  if (evt.getByLabel(fNuGenModuleLabel, fluxHandle))
+    art::fill_ptr_vector(flux, fluxHandle);
+  else
+    mf::LogWarning("NueCutSelection") << "No MCFlux";
+
+  // Chuck out a warning if there are multiple truths (i.e. multiple neutrinos)
+  if (mcTruth.size() > 1) {
+    mf::LogWarning("NueCutSelection") << "There are " << mcTruth.size() << " MCTruth in this event.  Only taking the first one!!!!";
     std::cout<<"nice"<<std::endl;
   }
-}
 
-DEFINE_ART_MODULE(FDSelection::NueCutSelection)
+  // Get the neutrino
+  const simb::MCNeutrino& mcneutrino = mcTruth.at(0)->GetNeutrino();
+  const simb::MCParticle& nu = mcneutrino.Nu();
+  const simb::MCParticle& lepton = mcneutrino.Lepton();
+
+  // Neutrino stuff
+  fNuPdg	= nu.PdgCode();
+  fBeamPdg	= flux.at(0)->fntype;
+  fNC		= mcneutrino.CCNC();
+  fMode		= mcneutrino.Mode();
+  fQ2		= mcneutrino.QSqr();
+  fEnu		= nu.E(); 
+  fW		= mcneutrino.W(); 
+  fX		= mcneutrino.X();
+  fY		= mcneutrino.Y();
+  fNuMomX	= nu.Momentum().X();
+  fNuMomY	= nu.Momentum().Y();
+  fNuMomZ	= nu.Momentum().Z();
+  fNuMomT	= nu.Momentum().T();
+  fNuX		= nu.Position().X();
+  fNuY		= nu.Position().Y();
+  fNuZ		= nu.Position().Z();
+  fNuT		= nu.Position().Y();
+  // Outgoing lepton stuff
+  fLepPDG	= lepton.PdgCode();
+  fLepMomX	= lepton.Momentum().X();
+  fLepMomY	= lepton.Momentum().Y();
+  fLepMomZ	= lepton.Momentum().Z();
+  fLepMomT	= lepton.Momentum().T();
+  fLepNuAngle	= nu.Momentum().Vect().Angle(lepton.Momentum().Vect());
+
+  return;
+
+}
---
+++ dune/FDSelections/PIDAnaAlg.cxx
@@ -0,0 +1,228 @@
+///////////////////////////////////////////////
+// PIDAnaAlg.cxx
+//
+// Reco and true PID stuff up
+// D Brailsford & M Wallbank, June 2017
+///////////////////////////////////////////////
+
+#include "PIDAnaAlg.h"
+
+FDSelection::PIDAnaAlg::PIDAnaAlg(const fhicl::ParameterSet& pset) {
+  fTrackModuleLabel  = pset.get<std::string>("TrackModuleLabel");
+  fShowerModuleLabel = pset.get<std::string>("ShowerModuleLabel");
+  fPIDModuleLabel    = pset.get<std::string>("PIDModuleLabel");
+  this->InitialiseTree();
+}
+
+void FDSelection::PIDAnaAlg::InitialiseTree() {
+
+  fTree = tfs->make<TTree>("PIDAna","PIDAna");
+  fTree->Branch("NObjects",    &fNObjects);
+  fTree->Branch("Track",       fTrack,       "fTrack[NObjects]/B");
+  fTree->Branch("Shower",      fShower,      "fShower[NObjects]/B");
+  fTree->Branch("TruePDG",     fTruePDG,     "fTruePDG[NObjects]/I");
+  fTree->Branch("RecoPDG",     fRecoPDG,     "fRecoPDG[NObjects]/I");
+  fTree->Branch("ElectronMVA", fElectronMVA, "fElectronMVA[NObjects]/D");
+  fTree->Branch("MuonMVA",     fMuonMVA,     "fMuonMVA[NObjects]/D");
+  fTree->Branch("PhotonMVA",   fPhotonMVA,   "fPhotonMVA[NObjects]/D");
+  fTree->Branch("ProtonMVA",   fProtonMVA,   "fProtonMVA[NObjects]/D");
+  fTree->Branch("PionMVA",     fPionMVA,     "fPionMVA[NObjects]/D");
+  fTree->Branch("ObjPurity",   fObjPurity,   "fObjPurity[NObjects]/D");
+  fTree->Branch("Primary",     fPrimary,     "fPrimary[NObjects]/B");
+  fTree->Branch("TrueEnergy",  fTrueEnergy,  "fTrueEnergy[NObjects]/D");
+  fTree->Branch("RecoEnergy",  fRecoEnergy,  "fRecoEnergy[NObjects]/D");
+  fTree->Branch("TrueX",       fTrueX,       "fTrueX[NObjects]/D");
+  fTree->Branch("TrueY",       fTrueY,       "fTrueY[NObjects]/D");
+  fTree->Branch("TrueZ",       fTrueZ,       "fTrueZ[NObjects]/D");
+  fTree->Branch("RecoX",       fRecoX,       "fRecoX[NObjects]/D");
+  fTree->Branch("RecoY",       fRecoY,       "fRecoY[NObjects]/D");
+  fTree->Branch("RecoZ",       fRecoZ,       "fRecoZ[NObjects]/D");
+  fTree->Branch("RecoLength",  fRecoLength,  "fRecoLength[NObjects]/D");
+  fTree->Branch("RecoPoints",  fRecoPoints,  "fRecoPoints[NObjects]/I");
+
+}
+
+void FDSelection::PIDAnaAlg::Run(const art::Event& evt) {
+
+  std::cout << std::endl << "Running PIDAna for event " << evt.event() << std::endl;
+
+  // Get tracks
+  art::Handle<std::vector<recob::Track> > trackHandle;
+  std::vector<art::Ptr<recob::Track> > tracks;
+  if (evt.getByLabel(fTrackModuleLabel, trackHandle))
+    art::fill_ptr_vector(tracks, trackHandle);
+  else
+    mf::LogWarning("PIDAnaAlg") << "No tracks";
+
+  // Get showers
+  art::Handle<std::vector<recob::Shower> > showerHandle;
+  std::vector<art::Ptr<recob::Shower> > showers;
+  if (evt.getByLabel(fShowerModuleLabel, showerHandle))
+    art::fill_ptr_vector(showers, showerHandle);
+  else
+    mf::LogWarning("PIDAnaAlg") << "No showers";
+
+  // Hit associations
+  art::FindManyP<recob::Hit> fmht(trackHandle, evt, fTrackModuleLabel);
+  art::FindManyP<recob::Hit> fmhs(showerHandle, evt, fShowerModuleLabel);
+
+  // PID particle associations
+  art::FindManyP<anab::MVAPIDResult> fmpidt(trackHandle, evt, fPIDModuleLabel);
+  art::FindManyP<anab::MVAPIDResult> fmpids(showerHandle, evt, fPIDModuleLabel);
+
+  // true particles
+  const sim::ParticleList& trueParticleList = bt->ParticleList();
+
+  // reco
+  //fNObjects = (int)tracks.size() + (int)showers.size(); // for the moment, don't assume all objects have associated pid
+  int i_obj = 0;
+
+  // tracks
+  for (std::vector<art::Ptr<recob::Track> >::const_iterator trackIt = tracks.begin(); trackIt != tracks.end(); ++trackIt) {
+    const std::vector<art::Ptr<recob::Hit> > hits = fmht.at(trackIt->key());
+    int trueParticle = TrueParticle(hits);
+    if (!trueParticleList.HasParticle(trueParticle))
+      continue;
+    const std::map<int,double> trueParticles = TrueParticles(hits);
+    double chargeTot = 0;
+    for (std::map<int,double>::const_iterator trueParticleIt = trueParticles.begin(); trueParticleIt != trueParticles.end(); ++trueParticleIt)
+      chargeTot += trueParticleIt->second;
+    double purity = trueParticles.at(trueParticle)/chargeTot;
+    std::vector<art::Ptr<anab::MVAPIDResult> > pids = fmpidt.at(trackIt->key());
+    // std::cout << std::endl << "Track " << trackIt->key() << std::endl;
+    // std::cout << "  Associated with particles:" << std::endl;
+    // for (std::map<int,double>::const_iterator trueParticlesIt = trueParticles.begin(); trueParticlesIt != trueParticles.end(); ++trueParticlesIt)
+    //   std::cout << "    " << trueParticlesIt->first << " (PDG " << trueParticleList[trueParticlesIt->first]->PdgCode() << ") contributes charge " << trueParticlesIt->second << std::endl;
+    // std::cout << "  True particle assumed to be " << trueParticle << " (PDG " << trueParticleList[trueParticle]->PdgCode() << ")" << std::endl;
+    // std::cout << "  There are " << pids.size() << " mva pid result objects association with track " << trackIt->key() << std::endl;
+    // for (std::vector<art::Ptr<anab::MVAPIDResult> >::const_iterator pidIt = pids.begin(); pidIt != pids.end(); ++pidIt) {
+    //   const std::map<std::string,double> mvaOutput = (*pidIt)->mvaOutput;
+    //   std::cout << "    PID " << pidIt->key() << " has " << mvaOutput.size() << " MVA outputs" << std::endl;
+    //   for (std::map<std::string,double>::const_iterator mvaOutIt = mvaOutput.begin(); mvaOutIt != mvaOutput.end(); ++mvaOutIt)
+    // 	std::cout << "      MVA out " << std::distance<std::map<std::string,double>::const_iterator>(mvaOutput.begin(),mvaOutIt) << " is " << mvaOutIt->first << " : " << mvaOutIt->second << std::endl;
+    // }
+    if (!pids.size())
+      continue;
+    const simb::MCParticle* mcparticle = trueParticleList.at(trueParticle);
+    std::map<std::string,double> mvaOutMap = pids.at(0)->mvaOutput;
+    // fill tree variables
+    fTrack[i_obj]       = true;
+    fShower[i_obj]   	= false;
+    fTruePDG[i_obj]    	= mcparticle->PdgCode();
+    fRecoPDG[i_obj]    	= 13;//tmp
+    fElectronMVA[i_obj] = mvaOutMap["electron"];
+    fMuonMVA[i_obj]	= mvaOutMap["muon"]; 
+    fPhotonMVA[i_obj]   = mvaOutMap["photon"];
+    fProtonMVA[i_obj]   = mvaOutMap["proton"];
+    fPionMVA[i_obj]     = mvaOutMap["pich"];
+    fObjPurity[i_obj]   = purity;
+    fPrimary[i_obj]     = mcparticle->Process() == "primary";
+    fTrueEnergy[i_obj]  = mcparticle->E();
+    fRecoEnergy[i_obj]  = (*trackIt)->StartMomentum();
+    fTrueX[i_obj]       = mcparticle->Position().X();
+    fTrueY[i_obj]       = mcparticle->Position().Y();
+    fTrueZ[i_obj]       = mcparticle->Position().Z();
+    fRecoX[i_obj]       = (*trackIt)->Vertex().X();
+    fRecoY[i_obj]       = (*trackIt)->Vertex().Y();
+    fRecoZ[i_obj]       = (*trackIt)->Vertex().Z();
+    fRecoLength[i_obj]  = (*trackIt)->Length();
+    fRecoPoints[i_obj]  = (*trackIt)->NumberTrajectoryPoints();
+    ++i_obj;
+  }
+
+  // showers
+  for (std::vector<art::Ptr<recob::Shower> >::const_iterator showerIt = showers.begin(); showerIt != showers.end(); ++showerIt) {
+    const std::vector<art::Ptr<recob::Hit> > hits = fmhs.at(showerIt->key());
+    int trueParticle = TrueParticle(hits);
+    if (!trueParticleList.HasParticle(trueParticle))
+      continue;
+    const std::map<int,double> trueParticles = TrueParticles(hits);
+    double chargeTot = 0;
+    for (std::map<int,double>::const_iterator trueParticleIt = trueParticles.begin(); trueParticleIt != trueParticles.end(); ++trueParticleIt)
+      chargeTot += trueParticleIt->second;
+    double purity = trueParticles.at(trueParticle)/chargeTot;
+    std::vector<art::Ptr<anab::MVAPIDResult> > pids = fmpids.at(showerIt->key());
+    if (!pids.size())
+      continue;
+    const simb::MCParticle* mcparticle = trueParticleList.at(trueParticle);
+    std::map<std::string,double> mvaOutMap = pids.at(0)->mvaOutput;
+    // fill tree variables
+    fTrack[i_obj]       = false;
+    fShower[i_obj]   	= true;
+    fTruePDG[i_obj]    	= mcparticle->PdgCode();
+    fRecoPDG[i_obj]    	= 11;//tmp
+    fElectronMVA[i_obj] = mvaOutMap["electron"];
+    fMuonMVA[i_obj]	= mvaOutMap["muon"]; 
+    fPhotonMVA[i_obj]   = mvaOutMap["photon"];
+    fProtonMVA[i_obj]   = mvaOutMap["proton"];
+    fPionMVA[i_obj]     = mvaOutMap["pich"];
+    fObjPurity[i_obj]   = purity;
+    fPrimary[i_obj]     = mcparticle->Process() == "primary";
+    fTrueEnergy[i_obj]  = mcparticle->E();
+    fRecoEnergy[i_obj]  = (*showerIt)->Energy().at((*showerIt)->best_plane());
+    fTrueX[i_obj]       = mcparticle->Position().X();
+    fTrueY[i_obj]       = mcparticle->Position().Y();
+    fTrueZ[i_obj]       = mcparticle->Position().Z();
+    fRecoX[i_obj]       = (*showerIt)->ShowerStart().X();
+    fRecoY[i_obj]       = (*showerIt)->ShowerStart().Y();
+    fRecoZ[i_obj]       = (*showerIt)->ShowerStart().Z();
+    fRecoLength[i_obj]  = -999.;//tmp, this has only just been added to recob::Shower so older code won't have it
+    fRecoPoints[i_obj]  = hits.size();
+    ++i_obj;
+  }
+
+  fNObjects = i_obj;
+
+  fTree->Fill();
+
+  return;
+
+}
+
+std::map<int,double> FDSelection::PIDAnaAlg::TrueParticles(const std::vector<art::Ptr<recob::Hit> >& hits) {
+
+  // Make a map of the tracks which are associated with this object and the charge each contributes
+  std::map<int,double> trackMap;
+  for (std::vector<art::Ptr<recob::Hit> >::const_iterator hitIt = hits.begin(); hitIt != hits.end(); ++hitIt) {
+    art::Ptr<recob::Hit> hit = *hitIt;
+    int trackID = ParticleID(hit);
+    trackMap[trackID] += hit->Integral();
+  }
+
+  return trackMap;
+
+}
+
+int FDSelection::PIDAnaAlg::TrueParticle(const std::vector<art::Ptr<recob::Hit> >& hits) {
+
+  std::map<int,double> trackMap = TrueParticles(hits);
+
+  // Pick the track with the highest charge as the 'true track'
+  double highestCharge = 0;
+  int objectTrack = 0;
+  for (std::map<int,double>::iterator trackIt = trackMap.begin(); trackIt != trackMap.end(); ++trackIt) {
+    if (trackIt->second > highestCharge) {
+      highestCharge = trackIt->second;
+      objectTrack  = trackIt->first;
+    }
+  }
+
+  return objectTrack;
+
+}
+
+int FDSelection::PIDAnaAlg::ParticleID(const art::Ptr<recob::Hit>& hit) {
+
+  double particleEnergy = 0;
+  int likelyTrackID = 0;
+  std::vector<sim::TrackIDE> trackIDs = bt->HitToTrackID(hit);
+  for (unsigned int idIt = 0; idIt < trackIDs.size(); ++idIt) {
+    if (trackIDs.at(idIt).energy > particleEnergy) {
+      particleEnergy = trackIDs.at(idIt).energy;
+      likelyTrackID = TMath::Abs(trackIDs.at(idIt).trackID);
+    }
+  }
+
+  return likelyTrackID;
+
+}
