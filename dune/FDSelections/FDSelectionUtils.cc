068eb6908f8fcc57896a39160b97d1a3b860b4bf
Dominic Brailsford <dbrailsf@fnal.gov>
2017-07-04 05:43:43 -0500

    Add skeleton which will hold selection utility functions and/or classes.  Currently holds a test function which successfully prints out (wow)

---
+++ dune/FDSelections/FDSelectionUtils.cc
@@ -0,0 +1,6 @@
+#include "FDSelectionUtils.h"
+
+void FDSelectionUtils::Test(){
+  std::cout<<"This is a test"<<std::endl;
+  return;
+}
---
+++ dune/FDSelections/FDSelectionUtils.h
@@ -0,0 +1,46 @@
+#ifndef FDSELECTIONUTILS_H_SEEN
+#define FDSELECTIONUTILS_H_SEEN
+
+
+///////////////////////////////////////////////
+// FDSelectionUtils.h
+//
+// A few selection utilities like truth matching 
+// D Brailsford & M Wallbank, June 2017
+///////////////////////////////////////////////
+
+// framework
+#include "art/Framework/Principal/Event.h"
+#include "fhiclcpp/ParameterSet.h" 
+#include "art/Framework/Principal/Handle.h" 
+#include "canvas/Persistency/Common/Ptr.h" 
+#include "canvas/Persistency/Common/PtrVector.h" 
+#include "art/Framework/Services/Registry/ServiceHandle.h" 
+#include "art/Framework/Services/Optional/TFileService.h" 
+#include "art/Framework/Services/Optional/TFileDirectory.h" 
+#include "messagefacility/MessageLogger/MessageLogger.h" 
+#include "canvas/Persistency/Common/FindManyP.h"
+
+// LArSoft
+#include "nusimdata/SimulationBase/MCParticle.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/Shower.h"
+//#include "lardataobj/AnalysisBase/MVAPIDResult.h"
+#include "lardataobj/AnalysisBase/ParticleID.h"
+#include "larsim/MCCheater/BackTracker.h"
+
+// c++
+#include <vector>
+#include <map>
+
+// ROOT
+#include "TTree.h"
+
+
+namespace FDSelectionUtils{
+  void Test();
+}
+
+#endif
